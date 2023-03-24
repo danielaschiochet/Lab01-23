9462a982c55c6e5570e9e819be5125b3204c0f0d
danys <danys@PC-di-Daniela.Home>
2023-03-24 21:54:33 +0100

    svolto

--- src/main/java/it/polito/tdp/Lab01/FXMLController.java
+++ src/main/java/it/polito/tdp/Lab01/FXMLController.java
@@ -20,35 +20,102 @@ public class FXMLController {
     @FXML
     private URL location;
 
-    @FXML
-    private Button btnInserisci;
+    @FXML // fx:id="btnCancella"
+    private Button btnCancella; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnInserisci"
+    private Button btnInserisci; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtParola"
+    private TextField txtParola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTempoEsecuzione"
+    private TextField txtTempoEsecuzione; // Value injected by FXMLLoader
+       
 
     @FXML
-    private Button btnReset;
-
-    @FXML
-    private TextField txtParola;
-
-    @FXML
-    private TextArea txtResult;
-
-    @FXML
-    void doInsert(ActionEvent event) {
+    void doCancella(ActionEvent event) {
+    	
+    	String p = this.txtResult.getSelectedText();
+    	
+    	double startTime = System.nanoTime();
+    	
+    	elenco.cancellaParola(p);
+    	
+    	double endTime = System.nanoTime();
+    	
+    	if(elenco.getElenco()!=null)
+    		this.txtResult.setText(elenco.getElenco().toString());
+    	else
+    		this.txtResult.clear();
+    	
+    	this.txtParola.clear();
+    	
+    	double time = (endTime - startTime)*0.000000001;
+    	
+    	this.txtTempoEsecuzione.setText("Operazione eseguita in "+time+"s");
 
     }
 
     @FXML
+    void doInsert(ActionEvent event) {
+    	
+    	this.txtResult.clear();
+    	
+    	String p = this.txtParola.getText();
+    	
+    	double startTime = System.nanoTime();
+    	
+    	elenco.addParola(p);
+    	
+    	double endTime = System.nanoTime();
+    	
+    	this.txtResult.setText(elenco.getElenco().toString());
+    	
+    	this.txtParola.clear();
+    	
+    	double time = (endTime - startTime)*0.000000001;
+    	
+    	this.txtTempoEsecuzione.setText("Operazione eseguita in "+time+"s");
+    	
+    	
+    }
+
+    @FXML
     void doReset(ActionEvent event) {
+    	
+    	this.txtParola.clear();
+    	this.txtResult.clear();
+    	this.txtTempoEsecuzione.clear();
+    	
+    	double startTime = System.nanoTime();
+    	
+    	elenco.reset();
+    	
+    	double endTime = System.nanoTime();
+    	
+    	double time = (endTime - startTime)*0.000000001;
+    	
+    	this.txtTempoEsecuzione.setText("Operazione eseguita in "+time+"s");
+    	
 
     }
 
     @FXML
     void initialize() {
-        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
-        
+    	  assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Scene.fxml'.";
+          assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+          assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+          assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+          assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+          assert txtTempoEsecuzione != null : "fx:id=\"txtTempoEsecuzione\" was not injected: check your FXML file 'Scene.fxml'.";
+
         elenco = new Parole() ;
     }
 
--- src/main/java/it/polito/tdp/Lab01/model/Parole.java
+++ src/main/java/it/polito/tdp/Lab01/model/Parole.java
@@ -1,24 +1,62 @@
 package it.polito.tdp.Lab01.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
+	
+	private List<String> words;
 		
 	public Parole() {
 		//TODO
+		//words = new ArrayList<String>();
+		words = new LinkedList<String>();
 	}
 	
 	public void addParola(String p) {
 		//TODO
+		this.words.add(p);
 	}
 	
 	public List<String> getElenco() {
 		//TODO
+		//List<String> wordsOrdineAlfabetico = new ArrayList<>();
+		List<String> wordsOrdineAlfabetico = new LinkedList<>();
+		if(!words.isEmpty()) {
+			for(String s: words) {
+				wordsOrdineAlfabetico.add(s);
+			}
+			Collections.sort(wordsOrdineAlfabetico, new Parole.ComparatoreParole());
+			
+			return wordsOrdineAlfabetico;
+		}
+
 		return null;
 	}
 	
 	public void reset() {
 		// TODO
+		words.clear();
+	}
+	
+	public void cancellaParola(String p) {
+		
+		words.remove(p);
+	}
+	
+	static class ComparatoreParole implements Comparator<String>{
+			
+			public int compare(String arg0, String arg1){
+				if(arg0.compareTo(arg1)<0)
+					return -1;
+				else if(arg0.compareTo(arg1)>0)
+					return 1;
+			return 0;
+			}
 	}
 
+	
 }
